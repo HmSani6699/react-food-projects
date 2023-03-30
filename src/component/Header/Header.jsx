dda2b484538bd28d0aa50a9279bf6a04463e0fbf
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-30 12:36:31 +0600

    ad to author menu ber

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
     return (
-        <div className='header-container'>
-            <h3 className='header-logo'><span className='author-logo'>Food</span> web</h3>
-            <nav>
-                <a href="/home">Home</a>
-                <a href="/home">Abut us</a>
-                <a href="/home">Blog</a>
-                <a href="/home">Contact us</a>
-            </nav>
+        <div >
+            <div className='header-container'>
+                <h3 className='header-logo'><span className='author-logo'>Food</span> web</h3>
+                <nav>
+                    <a href="/home">Home</a>
+                    <a href="/home">Abut us</a>
+                    <a href="/home">Blog</a>
+                    <a href="/home">Contact us</a>
+                </nav>
+            </div>
+            <div className='author-menu'>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
         </div>
     );
 };
